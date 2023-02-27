6f901470d84df20bd771d7852d9c22214dcae978
Skafii <piotrtkaczyk21a@wp.pl>
2023-02-27 12:49:31 +0100

    zainicjowanie projektu

---
+++ ExampleProject/src/test/java/com/ExampleProject/ExampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ExampleProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
